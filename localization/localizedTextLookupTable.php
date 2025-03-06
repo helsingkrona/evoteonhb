0e19672eebfbbed4233243844bc1f9505d535748
GeorgHI <hansssnah666@gmail.com>
2025-03-06 14:29:35 +0100

    bug fix

--- localization/localizedTextLookupTable.php
+++ localization/localizedTextLookupTable.php
@@ -480,8 +480,8 @@ define("LOCALIZED_TEXT_LOOKUP_TABLE", array(
         "scanian"=>"Ejn vaulomgong é rejdan igång",
     ),
     " casted votes so far"=>array(
-        "sv"=>" röster har inkommit"
-        "scanian"=>" röster har inkommit"
+        "sv"=>" röster har inkommit",
+        "scanian"=>" röster har inkommit",
     ),
     "You must enter at least two candidates"=>array(
         "sv"=>"Du måste ange minst två kandidater",
